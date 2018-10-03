fbaa6f2b8edeab5d0ba9356e2de4b297caf73462
Arsalan <arsnag03@gmail.com>
2018-10-04 00:39:47 +0300

    fix

--- index.php
+++ index.php
@@ -1,30 +1,45 @@
 <?php
 
-require 'vendor/altorouter/AltoRouter.php';
+require 'vendor/altorouter/altorouter/AltoRouter.php';
 require 'src/config.php';
 
-if ($GLOBALS['config']['is_debug_mode']) {
-    ini_set('error_reporting', E_ALL);
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
+use Pimple\Container;
+
+initContainers();
+initConfigs();
+initRouting();
+
+
+function initContainers() {
+    $container = new Container();
 }
 
-$router = new AltoRouter();
-$router->setBasePath('');
-$router->map('GET', '/',     'www/index.php',           'home');
-$router->map('POST','/auth', ['controller' => '/src/Controller/Auth.php', 'action' => 'authorization'], 'auth');
-
-$match = $router->match();
-
-if ($match === false) {
-    echo "// here you can handle 404 \n";
-} else {
-    if (is_array($match['target'])) {
-        $controller = new $match['target']['controller']();
-        $actionName = $match['target']['action'];
-        $params = $match['params'];
-    } else {
-        require $match['target'];
+function initConfigs() {
+    if ($GLOBALS['config']['is_debug_mode']) {
+        ini_set('error_reporting', E_ALL);
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
     }
+}
 
+function initRouting() {
+    $router = new AltoRouter();
+    $router->setBasePath('');
+    $router->map('GET', '/',     'www/index.php',           'home');
+    $router->map('POST','/auth', ['controller' => '/src/Controller/Auth.php', 'action' => 'authorization'], 'auth');
+
+    $match = $router->match();
+
+    if ($match === false) {
+        echo "// here you can handle 404 \n";
+    } else {
+        if (is_array($match['target'])) {
+            $controller = new $match['target']['controller']();
+            $actionName = $match['target']['action'];
+            $params = $match['params'];
+            /** @TODO something */
+        } else {
+            require $match['target'];
+        }
+    }
 }
